52f05a519292a804d8c6fbd7f53d46c4c25aa030
Alexander Gorbunov <alexander.gorbunov@noveogroup.com>
2015-04-09 09:39:38 +0600

    Added a template for example project.

---
+++ NOAPIControllerExample/NOAPIControllerExample/AppDelegate.m
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.m
+//  NOAPIControllerExample
+//
+//  Created by Alexander Gorbunov on 09/04/15.
+//  Copyright (c) 2015 Noveo. All rights reserved.
+//
+
+
+#import "AppDelegate.h"
+
+
+@interface AppDelegate ()
+@end
+
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    // Override point for customization after application launch.
+    return YES;
+}
+
+@end
---
+++ NOAPIControllerExample/NOAPIControllerExample/ViewController.m
@@ -0,0 +1,25 @@
+//
+//  ViewController.m
+//  NOAPIControllerExample
+//
+//  Created by Alexander Gorbunov on 09/04/15.
+//  Copyright (c) 2015 Noveo. All rights reserved.
+//
+
+
+#import "ViewController.h"
+
+
+@interface ViewController ()
+@end
+
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+@end
